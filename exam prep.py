0203082bbc5a2b24c0658a05a355cf35bcca622b
kaizen <akandepelumi102@gmail.com>
2025-03-08 22:20:54 +0000

    added file

---
+++ exam prep.py
@@ -0,0 +1,11 @@
+# x  = True
+# y = False 
+# print("x or y is", not y)
+
+number = 99
+print((4m)(lambda x, y:x+y)
+
+# while number < 125:
+#     new_number = number + 1
+#     number = new_number
+#     print(number + 1)
