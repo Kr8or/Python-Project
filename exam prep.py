d2a62e6fccaf1639149312a836dde8579718c7c5
kaizen <akandepelumi102@gmail.com>
2025-03-11 23:05:32 +0000

    added file

--- exam prep.py
+++ exam prep.py
@@ -5,9 +5,12 @@
 # for i in range(100, 126, 2):
 #     print(i)
 
-# while number < 125:
-#     number = number + 1
-#     print(number)
+number = 99
+
+while number < 125:
+    print(number)
+    number = number + 1
+
 
 # lists = [1, 30, 40, 50, 60, 70, 100, 920]
 # lists.sort()
@@ -23,10 +26,39 @@
 #         fruit_count[fruit] = 1
 # print(fruit_count)
 
-def area_tri(length, breath):
-    area = 0.5 * length * breath
-    print(area)
+# def area_tri(length, breath):
+#     area = 0.5 * length * breath
+#     print(area)
 
-user_input_1 = int(input("Enter the length of the triangle: "))
-user_input_2 = int(input("Enter the breath of the triangle: "))
-area_tri(user_input_1, user_input_2) 
+# user_input_1 = int(input("Enter the length of the triangle: "))
+# user_input_2 = int(input("Enter the breath of the triangle: "))
+# area_tri(user_input_1, user_input_2) 
+
+# def temp_conv(temp_fehrenheit):
+#     celsius = (temp_fehrenheit - 32) * 5/9
+#     print(celsius)
+
+# temperature = int(input("what is the temoerature in farhrenheit: "))
+
+# temp_conv(temperature)
+
+# data = [2, 3, 4, 5, 6, 7, 8, 12, 16, 20]
+
+# def calculate_average(list_of_data):
+#     if not list_of_data:
+#         return None
+    
+#     sum_numbers = sum(list_of_data)
+#     average = sum_numbers/len(list_of_data)
+#     print(average)
+    
+# calculate_average(data)
+    
+    
+# artimetic, comparism and logical operators
+# control flow is the order which a program's code executes (control structure)
+# sequential, selection and repetition 
+# selection statments (decision control statments)if statments, if-elif, if-elif-else 
+# selection runs varius conditions and execute the one that is true
+# repetition for loop & while loop it repets a block of code while a condition is true
+# data structures (lists, tuples, dictionaries, set)
