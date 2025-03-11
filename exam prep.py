ca83b1974de5f611e25dffeae69108caee300340
kaizen <akandepelumi102@gmail.com>
2025-03-11 00:28:10 +0000

    added file

--- exam prep.py
+++ exam prep.py
@@ -9,8 +9,24 @@
 #     number = number + 1
 #     print(number)
 
-lists = [1, 30, 40, 50, 60, 70, 100, 920]
-lists.sort()
-print(lists[0])
+# lists = [1, 30, 40, 50, 60, 70, 100, 920]
+# lists.sort()
+# print(lists[0])
 
-fruit_list = ['apple', 'orange', 'pear', 'bannan']
+# fruit_list = ['apple', 'orange', 'pear', 'bannan', 'apple', 'apple', 'bannan', 'pear', 'orange']
+# fruit_count = {}
+
+# for fruit in fruit_list:
+#     if fruit in fruit_count:
+#         fruit_count[fruit] += 1
+#     else:
+#         fruit_count[fruit] = 1
+# print(fruit_count)
+
+def area_tri(length, breath):
+    area = 0.5 * length * breath
+    print(area)
+
+user_input_1 = int(input("Enter the length of the triangle: "))
+user_input_2 = int(input("Enter the breath of the triangle: "))
+area_tri(user_input_1, user_input_2) 
